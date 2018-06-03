6de7d205b6f245fd23b199407653697fd4d47436
Ryerson Student <ryersonstudent@ryersons-mac.local>
2018-06-03 02:23:02 -0400

    Initial Commit

---
+++ ECFindMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ECFindMe
+//
+//  Created by Ryerson Student on 2018-06-03.
+//  Copyright © 2018 Ryerson Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
